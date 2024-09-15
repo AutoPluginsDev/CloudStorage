8016f87661c0387b979cecdcf4580b5a32ff0c42
RonanTR <74414575+lumi-git@users.noreply.github.com>
2024-09-15 20:29:38 +0200

    Initial commit

---
+++ src/main/java/fr/lumi/Util/JoinServer.java
@@ -0,0 +1,29 @@
+package fr.lumi.Util;
+
+import fr.lumi.Main;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.entity.Player;
+
+
+public class JoinServer implements Listener {
+    Main plugin;
+
+    public JoinServer(Main plg) {
+        plugin = plg;
+    }
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        if (player.isOp() || player.hasPermission("acmd.admin"))
+        {
+            // Todo
+            // if (plugin.getConfig().getBoolean("CheckVersion"))
+            if (Main.isNewVersionAvailable())
+                player.sendMessage(plugin.VerifyPluginVersion());
+
+        }
+    }
+}
